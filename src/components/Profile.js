1463c45b81ea6812584bfadf6b813ae26fea6a78
Matthew Lau <matthew.lau78@gmail.com>
2021-03-24 02:54:56 +1100

    profile.js

---
+++ src/components/Profile.js
@@ -0,0 +1,8 @@
+const Profile = {
+    render: () => {
+        const profile = document.createElement("div");
+        profile.id = "profile";
+        profile.className = "container-md justify-content-center feed";
+        return profile;
+    },
+};
